84257b1f14fdbe36adc19c59d2d5b001de0f6ecf
Paweł Pietrzak <ppietrzak99@gmail.com>
2026-05-18 20:49:18 +0200

    dodana obsługa silników obliczeniowych i skompilowane silniki

---
+++ SilnikAlgorytmow.java
@@ -0,0 +1,50 @@
+package proj;
+
+import java.io.IOException;
+import java.io.File;
+import java.io.InterruptedIOException;
+
+public class SilnikAlgorytmow {
+    /**
+     * Metoda wywołuje zewnętrzny program w C.
+     * @param plik Oryginalny plik z krawędziami (np. graf.txt)
+     * @param algortym Nazwa algorytmu do przekazania w parametrze (np. "Fruchterman-Reingold")
+     * @return Plik wynikowy wygenerowany przez program w C
+     */
+    public static File uruchomAlgorytm(File plik, String algortym) throws IOException, InterruptedException {
+        // Sprawdzenie na jakim systemie aktualnie działa Java
+        String systemOperacyjny = System.getProperty("os.name").toLowerCase();
+
+        // Wybór nazwy pliku w zależności od systemu
+        String nazwaSilnika;
+        if (systemOperacyjny.contains("win")) {
+            nazwaSilnika = "silnik.exe"; // Windows
+        } else {
+            nazwaSilnika = "./silnik";   // Linux
+        }
+
+        String plikWynikowy = "temp.txt";
+
+        // Przygotowanie procesu
+        ProcessBuilder konstruktorProcesu = new ProcessBuilder(
+                nazwaSilnika,
+                "-i", plik.getAbsolutePath(),
+                "-o", plikWynikowy,
+                "-a", algortym
+        );
+
+        // logger silnika
+        konstruktorProcesu.inheritIO();
+
+        // Uruchomienie silnika
+        try (Process silnik = konstruktorProcesu.start()) {
+
+            int kodPowrotu = silnik.waitFor();
+            if (kodPowrotu != 0) {
+                System.err.println("Silnik obliczeniowy zwrócił błąd. Kod wyjścia: " + kodPowrotu);
+            }
+        }
+
+        return new File(plikWynikowy);
+    }
+}
