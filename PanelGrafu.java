eeaaedcfa27d1e6a5367f226e103c4e508c0152e
Paweł Pietrzak <ppietrzak99@gmail.com>
2026-05-18 20:52:07 +0200

    dodany zoom ze scrolla, przesuwanie wierzchołków i przesuwanie tła prawym przyciskiem myszy

--- PanelGrafu.java
+++ PanelGrafu.java
@@ -5,6 +5,7 @@ package proj;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.*;
+import java.awt.event.*;
 
 /**
  * Komponent Swing odpowiedzialny za renderowanie grafu.
@@ -12,10 +13,27 @@ import java.awt.geom.*;
 public class PanelGrafu extends JPanel {
     private ModelGrafu model;
     private double przyblizenie = 1.0;
+    private Wierzcholek aktywnyWierzcholek = null;
+    private double przesuniecieX = 0.0;
+    private double przesuniecieY = 0.0;
+    private int ostatniaPozX;
+    private int ostatniaPozY;
 
     public PanelGrafu(ModelGrafu model) {
         this.model = model;
         setBackground(Color.WHITE);
+
+        ObslugaMyszy obslugiwacz = new ObslugaMyszy();
+        addMouseListener(obslugiwacz);
+        addMouseMotionListener(obslugiwacz);
+        addMouseWheelListener(e -> {
+            if (e.getWheelRotation() < 0) {
+                przyblizenie *= 1.1; // zoom o 10%
+            } else {
+                przyblizenie /= 1.1;
+            }
+            repaint();
+        });
     }
 
     public void ustawPrzyblizenie(double wartosc) {
@@ -31,9 +49,9 @@ public class PanelGrafu extends JPanel {
         // Włączenie antyaliasingu (wygładzanie krawędzi)
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        // Skalowanie widoku
-        AffineTransform at = AffineTransform.getScaleInstance(przyblizenie, przyblizenie);
-        g2.transform(at);
+        // Przesunięcie i skalowanie kamery
+        g2.translate(przesuniecieX, przesuniecieY);
+        g2.scale(przyblizenie, przyblizenie);
 
         // 1. Rysowanie krawędzi
         g2.setColor(Color.LIGHT_GRAY);
@@ -53,6 +71,49 @@ public class PanelGrafu extends JPanel {
 
             g2.setColor(Color.BLACK);
             g2.drawString(w.getNazwa(), (int)w.getX() + 7, (int)w.getY() - 7);
+        }
+    }
+
+    private class ObslugaMyszy extends MouseAdapter {
+        @Override
+        public void mousePressed(MouseEvent e) {
+            ostatniaPozX = e.getX();
+            ostatniaPozY = e.getY();
+            for (Wierzcholek w : model.getWierzcholki().values()) {
+                double myszX = (e.getX() - przesuniecieX) / przyblizenie;
+                double myszY = (e.getY() - przesuniecieY) / przyblizenie;
+                double odleglosc = Math.sqrt((w.getX() - myszX)*(w.getX() - myszX)
+                        + (w.getY() - myszY)*(w.getY() - myszY));
+                // promień wierzchołka wynosi 5, ale będzie łatwiej kliknąć
+                if (odleglosc <= 10 && SwingUtilities.isLeftMouseButton(e)) {
+                    aktywnyWierzcholek = w;
+                    break;
+                }
+            }
+        }
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            if (aktywnyWierzcholek != null) {
+                double pozX = (e.getX() - przesuniecieX) / przyblizenie;
+                double pozY = (e.getY() - przesuniecieY) / przyblizenie;
+                aktywnyWierzcholek.setX(pozX);
+                aktywnyWierzcholek.setY(pozY);
+            } else if (!SwingUtilities.isLeftMouseButton(e)){
+                double dx = e.getX() - ostatniaPozX;
+                double dy = e.getY() - ostatniaPozY;
+                przesuniecieX += dx;
+                przesuniecieY += dy;
+
+                ostatniaPozX = e.getX();
+                ostatniaPozY = e.getY();
+            }
+            repaint();
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            aktywnyWierzcholek = null;
         }
     }
 }
