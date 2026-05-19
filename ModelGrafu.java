497bfe8fba37d70ac0869d388d60b501bd554710
Paweł Pietrzak <ppietrzak99@gmail.com>
2026-05-20 01:58:05 +0200

    dodane uwzględnienie wag po obliczeniach wykonanych przez silnik

--- ModelGrafu.java
+++ ModelGrafu.java
@@ -20,24 +20,18 @@ public class ModelGrafu {
         this.krawedzie = new ArrayList<>();
     }
 
-    /**
-     * Czyści dane grafu przed wczytaniem nowego pliku.
-     */
+    // Czyszczenie danych przed wczytaniem nowego pliku
     public void wyczysc() {
         wierzcholki.clear();
         krawedzie.clear();
     }
 
-    /**
-     * Dodaje wierzchołek do modelu.
-     */
+    // Dodawanie danych o wierzchołkach do modelu
     public void dodajWierzcholek(Wierzcholek w) {
         wierzcholki.put(w.getNazwa(), w);
     }
 
-    /**
-     * Dodaje krawędź do modelu.
-     */
+    // Dodawanie danych o krawędziach do modelu
     public void dodajKrawedz(Krawedz k) {
         krawedzie.add(k);
     }
@@ -46,8 +40,40 @@ public class ModelGrafu {
     public Map<String, Wierzcholek> getWierzcholki() {
         return wierzcholki;
     }
-
     public List<Krawedz> getKrawedzie() {
         return krawedzie;
+    }
+
+    // Korygowanie współrzędnych wierzchołków o wagę krawędzi za pomocą algorytmu siłowego
+    public void korygujWspolrzedneWagami() {
+        int liczbaWierzcholkow = this.wierzcholki.size();
+        int iteracje = Math.max(50, liczbaWierzcholkow * 10);
+        double poczatkowaTemperatura = 0.05;
+        for (int i = 0; i < iteracje; i++) {
+            double aktualnaTemperatura = poczatkowaTemperatura *(1.0 - (double) i/iteracje);
+            for (Krawedz k : this.getKrawedzie()) {
+                Wierzcholek w1 = k.getV1();
+                Wierzcholek w2 = k.getV2();
+
+                double dx = w2.getX() - w1.getX();
+                double dy = w2.getY() - w1.getY();
+                double odleglosc = Math.sqrt(dx*dx + dy*dy);
+
+                if (odleglosc < 1.0) continue;
+                double waga = k.getWaga();
+                double silaKorekty = aktualnaTemperatura*(waga - 1.0);
+
+                double kierunekSilyX = dx/odleglosc;
+                double kierunekSilyY = dy/odleglosc;
+
+                double przesuniecieX = kierunekSilyX * silaKorekty * odleglosc;
+                double przesuniecieY = kierunekSilyY * silaKorekty * odleglosc;
+
+                w1.setX(w1.getX() + przesuniecieX);
+                w1.setY(w1.getY() + przesuniecieY);
+                w2.setX(w2.getX() - przesuniecieX);
+                w2.setY(w2.getY() - przesuniecieY);
+            }
+        }
     }
 }
